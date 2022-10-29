1fa382db69e58a527f70eedd09a149085327feff
MigoCode <micagel22gmail.com>
2022-10-29 19:51:31 +0200

    new sqlQueries

--- consequentialSQL/queriesTesting.sql
+++ consequentialSQL/queriesTesting.sql
@@ -69,7 +69,62 @@ DROP VIEW V1;
 -- 14.Query to Display User Tables
 SELECT * FROM customers WHERE Company_Name ='a';
 
+-- ===============================
 -- 15.Query to Display Primary Keys
 SELECT * FROM sql_query_tests.employees WHERE Emp_ID='PK';
 
-SELECT * FROM customers;
+-- 16.Query for Displaying Unique Keys
+SELECT * FROM sql_query_tests.employees WHERE Emp_ID='uq';
+
+-- 18.Displaying Triggers
+SELECT * FROM sql_query_tests.employees WHERE Emp_ID='tr';
+
+-- 19.Displaying Internal Tables
+SELECT * FROM sql_query_tests.employees WHERE Emp_ID='it';
+
+-- 20.Displaying a List of Procedures
+SELECT * FROM sql_query_tests.employees WHERE Emp_ID='p';
+
+-- 21.Swapping the Values of Two Columns in a table
+UPDATE Customers SET City=Region, Region=City;
+
+-- ===============================
+
+-- 22.Returning a Column of Unique Values
+SELECT DISTINCT Customer_ID FROM Customers;
+
+-- 23.Making a Top 20 with the SELECT TOP Clause
+SELECT TOP '20' FROM Customers WHERE Customer_ID<>NULL;
+
+-- 24.Searching for SQL Tables with Wildcards
+SELECT * From Customers 
+WHERE Company_Name 
+LIKE 'The%';
+
+-- 25.Born Between. Dates
+SELECT Emp_ID, Last_name 
+FROM employees 
+WHERE Date_of_birth 
+BETWEEN ‘1987-01-01’ AND ‘1996-01-07’;
+
+-- 26.Finding the Intersection of Two Tables
+SELECT Order_ID FROM orders 
+INNER JOIN customers 
+ON orders.order_ID = customers.customer_ID;
+
+-- 27.Doubling the Power with UNION
+SELECT Product_Name FROM products 
+UNION SELECT Ship_Name FROM orders;
+
+-- 28.Making Column Labels More Friendly
+SELECT Phone_Number AS contact FROM shippers;
+
+-- 29.Always and Everywhere!
+SELECT Product_Name FROM products 
+WHERE Product_ID = ALL
+(SELECT Product_ID FROM products
+WHERE Unit_price > 350);
+
+-- 30. Writing Developer Friendly SQL
+
+
